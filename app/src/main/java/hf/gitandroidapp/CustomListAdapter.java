0905e8c184a766517fb450f6fce37b042abd48c7
Amine Laazizi <amine.laazizi@gmail.com>
2019-05-15 01:45:19 +0000

    Modify ListAdapter Java Class

--- app/src/main/java/hf/gitandroidapp/CustomListAdapter.java
+++ app/src/main/java/hf/gitandroidapp/CustomListAdapter.java
@@ -1,8 +1,58 @@
 package hf.gitandroidapp;
 
 import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android.view.*;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class CustomListAdapter extends ArrayAdapter {
 
+    //to reference the Activity
+    private final Activity context;
 
+    //to store the user names
+    private final String[] usernameArray;
+
+    //to store the repo titles
+    private final String[] titleArray;
+
+    //to store the repo descriptions
+    private final String[] descArray;
+
+    //to store the ratings
+    private final Integer[] ratingArray;
+
+    public CustomListAdapter(Activity context, String[] usernameArrayParam, String[] tilteArrayParam, String[] descArrayParam, Integer[] ratingArrayParam){
+
+        super(context,R.layout.repoitem , usernameArrayParam);
+
+        this.context = context;
+        this.usernameArray = usernameArrayParam;
+        this.titleArray = tilteArrayParam;
+        this.descArray = descArrayParam;
+        this.ratingArray = ratingArrayParam;
+
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.repoitem, null,true);
+
+        //this code gets references to objects in the repoitem.xml file
+        TextView usernameTextField = (TextView) rowView.findViewById(R.id.repo_author);
+        TextView tilteTextField = (TextView) rowView.findViewById(R.id.repo_name);
+        TextView descTextField = (TextView) rowView.findViewById(R.id.repo_desc);
+        TextView ratingTextField = (TextView) rowView.findViewById(R.id.repo_rating);
+
+        //this code sets the values of the objects to values from the arrays
+        usernameTextField.setText(usernameArray[position]);
+        tilteTextField.setText(titleArray[position]);
+        descTextField.setText(descArray[position]);
+        ratingTextField.setText(ratingArray[position]);
+
+        return rowView;
+
+    };
 }
